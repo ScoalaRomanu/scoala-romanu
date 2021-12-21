c3755a541cf4c9693372b7f10c67de9b837e3dbd
Stefan Puia <stefan.puia13@gmail.com>
2021-12-21 11:21:44 +0200

    translations, featured pages

--- .eleventy.js
+++ .eleventy.js
@@ -7,17 +7,19 @@ module.exports = (config) => {
   config.addPassthroughCopy("./src/assets/css/main.min.css");
   config.addPassthroughCopy("./src/admin");
 
-  config.addFilter("formatDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString({ locale: process.env.LOCALE });
-  });
-  config.addFilter("formatDateTime", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_SHORT, {
-      locale: process.env.LOCALE,
-    });
-  });
+  // config.addFilter("formatDate", (dateObj) => {
+  //   return DateTime.fromJSDate(dateObj).toLocaleString({ locale: process.env.LOCALE });
+  // });
+  // config.addFilter("formatDateTime", (dateObj) => {
+  //   return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_SHORT, {
+  //     locale: process.env.LOCALE,
+  //   });
+  // });
   config.addFilter("sortMenuItems", (menuItems) => {
-    let values = [...menuItems];
-    return values.sort((a, b) => Math.sign(a.data.menuOrder - b.data.menuOrder));
+    return [...menuItems].sort((a, b) => Math.sign(a.data.menuOrder - b.data.menuOrder));
+  });
+  config.addFilter("featuredPages", (pages) => {
+    return [...pages].filter((page) => !!page.data.isFeatured);
   });
 
   return {
--- src/pages/about copy.md
+++ src/pages/about copy.md
@@ -1,9 +1,9 @@
 ---
-title: Despre noi
+title: Despre noi 2
 image: /images/pic09.jpg
 excerpt: Some description
-menuOrder: -10
-isFeatured: true
+menuOrder: 1
+isFeatured: false
 ---
 
 test
