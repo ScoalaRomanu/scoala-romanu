e85eac0a8570497c6a30a34ed61dd9f997cf12e6
Stefan Puia <stefan.puia13@gmail.com>
2021-12-21 09:46:09 +0200

    added cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,25 @@
-module.exports = (config) => {
-    config.addPassthroughCopy('./src/images');
-    config.addPassthroughCopy('./src/assets');
+import { DateTime } from "luxon";
 
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-        },
-    };
+export default (config) => {
+  config.addPassthroughCopy("./src/images");
+  config.addPassthroughCopy("./src/uploads");
+  config.addPassthroughCopy("./src/assets/js");
+  config.addPassthroughCopy("./src/assets/css/main.min.css");
+  config.addPassthroughCopy("./src/admin");
+
+  config.addFilter("formatDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString({ locale: process.env.LOCALE });
+  });
+  config.addFilter("formatDateTime", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_SHORT, {
+      locale: process.env.LOCALE,
+    });
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
---
+++ src/pages/about.md
@@ -0,0 +1,5 @@
+---
+title: Despre noi
+---
+
+test
