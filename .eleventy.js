2cfeac152df22dd12787f7dc61ce5ff7c5a433ec
Stefan Puia <stefan.puia13@gmail.com>
2021-12-21 18:33:28 +0200

    fixed markdown not rendering

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
-const { DateTime } = require("luxon");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { passthrough } = require("./eleventy/passthrough");
 const dateFilters = require("./eleventy/filter/date");
 const pageFilters = require("./eleventy/filter/pages");
 const nodeEnvShortcodes = require("./eleventy/shortcode/nodeEnv");
+const markdownPaired = require("./eleventy/shortcode/paired/markdown");
 
 module.exports = (config) => {
   passthrough.forEach((pt) => config.addPassthroughCopy(pt));
   [...dateFilters, ...pageFilters].forEach(([name, fn]) => config.addFilter(name, fn));
   [...nodeEnvShortcodes].forEach(([name, fn]) => config.addShortcode(name, fn));
+  [...markdownPaired].forEach(([name, fn]) => config.addPairedShortcode(name, fn));
   config.addPlugin(eleventyNavigationPlugin);
 
   return {
